9903812a0c2f6fc0e598dfe4715ca8c52f185440
Stephanie Glyzewski <stephanie.glyzewski@gmail.com>
2018-03-02 10:18:01 -0600

    thinking more about member variables

--- CustomListClassProject/CustomList.cs
+++ CustomListClassProject/CustomList.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace CustomListClassProject
 {
-   public class CustomList
+   public class CustomList <T>
+
     {
+        //member variables
+        public int count;
+        private T[] array;
+        //constructor
+        public CustomList()
+        {
+            count = 0;
+            array = new T[20];
+        }
+
+        //member methods
+        public T[] Add(T element)
+        {
+            
+            T[]array2 ={element};
+           
+            array = array + array2;
+            count++;
+            return array;
+
+        }
+
+        //indexer
+        public T this[int elementIndex]
+        {
+            get
+            {
+                return
+            }
+        }
+
+
+        public static CustomList<T> operator +(T[]array1, T[]array2)
+        {
+
+        }
+
+        public static CustomList<T> operator +(CustomList<T> list1, CustomList<T> list2)
+        {
+            CustomList<T> newList = new CustomList<T>()
+            newList
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
     }
-}
+    }
